2cf4db1ef06f3f3d64e1daf7843f856ca9d6feca
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-31 20:28:22 -0300

    Inicia implementacao do localStorage

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-// Requisito 4 - https://stackoverflow.com/questions/14984643/css-pick-a-random-color-from-array
-// https://stackoverflow.com/questions/58159610/how-to-stop-random-color-from-repeating
-let colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'grey', 'brown', 'orange', 'beige'];
-let paletteColor = document.querySelectorAll('.color');
-let btnRandomColor = document.getElementById('button-random-color');
+const colors = [
+  'red',
+  'green',
+  'blue',
+  'yellow',
+  'pink',
+  'purple',
+  'grey',
+  'brown',
+  'orange',
+  'beige',
+];
+const paletteColor = document.querySelectorAll('.color');
+const btnRandomColor = document.getElementById('button-random-color');
 
 function randomColor(event) {
-    event = colors[Math.floor(Math.random() * colors.length)];
+  let generate = event;
+  generate = colors[Math.floor(Math.random() * colors.length)];
 
-    return event;
+  return generate;
+}
+
+function colorsArray() {
+  const usedColors = [];
+
+  for (let i = 1; i < paletteColor.length; i += 1) {
+    usedColors.push(randomColor());
+  }
+
+  localStorage.setItem('colors', JSON.stringify(usedColors));
+  return usedColors;
 }
 
 function newColors() {
-    for (let i = 1; i < paletteColor.length; i += 1) {
-        paletteColor[i].style.backgroundColor = randomColor();
+  colorsArray();
+
+  for (let i = 1; i < paletteColor.length; i += 1) {
+    for (let j = 0; j < colorsArray.length; j += 1) {
+      paletteColor[i].style.backgroundColor = colorsArray[j];
     }
+  }
 }
 
-btnRandomColor.addEventListener('click', newColors)
+btnRandomColor.addEventListener('click', newColors);
+
+// Requisito 4 - https://stackoverflow.com/questions/14984643/css-pick-a-random-color-from-array
