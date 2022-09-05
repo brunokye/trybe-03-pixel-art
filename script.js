de95cd73a5c2ea4183d480a0774656f19f436aa4
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-05 17:23:55 -0300

    Adiciona o quadro de pixels

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const paletteColor = document.querySelectorAll('.color');
 const btnRandomColor = document.getElementById('button-random-color');
+const board = document.getElementById('pixel-board');
 let storageColors = [];
 
 // Random RGB - https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript
@@ -28,6 +29,20 @@ function newButtons() {
   }
 
   localStorage.setItem('colorPalette', JSON.stringify(storageColors));
-}
+} btnRandomColor.addEventListener('click', newButtons);
 
-btnRandomColor.addEventListener('click', newButtons);
+function pixelBoard() {
+  for (let i = 0; i < 5; i += 1) {
+    const lines = document.createElement('div');
+
+    board.appendChild(lines);
+    lines.className = 'line';
+
+    for (let j = 0; j < 5; j += 1) {
+      const pixels = document.createElement('div');
+
+      lines.appendChild(pixels);
+      pixels.className = 'pixel';
+    }
+  }
+} pixelBoard();
