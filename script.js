ad79bf102bd66a42c173f91dc1f8dbeeb7a8080d
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-05 19:26:19 -0300

    Adiciona funcao para selecionar uma cor

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const paletteColor = document.querySelectorAll('.color');
 const btnRandomColor = document.getElementById('button-random-color');
 const board = document.getElementById('pixel-board');
+const firstColor = paletteColor[0];
+const secondColor = paletteColor[1];
+const thirdColor = paletteColor[2];
+const fourthColor = paletteColor[3];
 let storageColors = [];
 
 // Random RGB - https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript
@@ -50,7 +54,51 @@ function pixelBoard() {
 } pixelBoard();
 
 function startSelected() {
-  const firstColor = document.querySelectorAll('.color')[0];
-
   firstColor.classList.add('selected');
 } startSelected();
+
+function firstSelected() {
+  if (firstColor.classList[1] === undefined) {
+    paletteColor.forEach((color) => {
+      color.classList.remove('selected');
+    });
+
+    firstColor.classList.add('selected');
+  }
+}
+
+function secondSelected() {
+  if (secondColor.classList[1] === undefined) {
+    paletteColor.forEach((color) => {
+      color.classList.remove('selected');
+    });
+
+    secondColor.classList.add('selected');
+  }
+}
+
+function thirdSelected() {
+  if (thirdColor.classList[1] === undefined) {
+    paletteColor.forEach((color) => {
+      color.classList.remove('selected');
+    });
+
+    thirdColor.classList.add('selected');
+  }
+}
+
+function fourthSelected() {
+  if (fourthColor.classList[1] === undefined) {
+    paletteColor.forEach((color) => {
+      color.classList.remove('selected');
+    });
+
+    fourthColor.classList.add('selected');
+  }
+}
+
+// Color selected - https://bobbyhadz.com/blog/javascript-remove-class-from-multiple-elements
+firstColor.addEventListener('click', firstSelected);
+secondColor.addEventListener('click', secondSelected);
+thirdColor.addEventListener('click', thirdSelected);
+fourthColor.addEventListener('click', fourthSelected);
